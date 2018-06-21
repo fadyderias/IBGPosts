61ebfe517324873598ac9ae445132f6f0252ee01
Fady <fadyderias_92@yahoo.com>
2018-06-21 03:38:39 +0200

    FEAT: Creating Post model object.

---
+++ IBGPosts/Networking/Models/Post.h
@@ -0,0 +1,21 @@
+//
+//  Post.h
+//  IBGPosts
+//
+//  Created by Fady on 6/16/18.
+//  Copyright © 2018 Instabug. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Post : NSObject
+
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *body;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IBGPosts/Networking/Models/Post.m
@@ -0,0 +1,15 @@
+//
+//  Post.m
+//  IBGPosts
+//
+//  Created by Fady on 6/16/18.
+//  Copyright © 2018 Instabug. All rights reserved.
+//
+
+#import "Post.h"
+
+@implementation Post
+
+
+
+@end
